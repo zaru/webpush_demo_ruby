4935b8d63c3f528d76e55a9495211d1727f21724
zaru <sakuraba@basicinc.jp>
2020-01-22 18:37:01 +0900

    implements webpush register script

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get '/' do
+    @public_key = File.read('./vapid_keys/public_key')
+    @private_key = File.read('./vapid_keys/private_key')
     erb :index
   end
 end
